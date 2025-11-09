5a3c01a6bcf3e790293a9c6df96b8d0853c054e5
Agaton Pourshahidi <166090825+agatonpour@users.noreply.github.com>
2025-11-09 23:46:25 +0000

    Change refresh button to load
    Update the text of the refresh button on the status and environmental pages to "Load". Also, remove unused serialport files to resolve build errors.

--- src/pages/Status.tsx
+++ src/pages/Status.tsx
@@ -192,7 +192,7 @@ export default function Status() {
                 ) : (
                   <span className="flex items-center">
                     <RefreshCw className="h-4 w-4 mr-2" />
-                    Refresh
+                    Load
                   </span>
                 )}
               </Button>
